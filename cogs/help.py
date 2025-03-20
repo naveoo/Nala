73057dd7fb4d069f60f56697653ba883bd05a223
naveo <naa.veoos@gmail.com>
2025-03-20 19:14:04 +0100

    Mise à jour de la commande /help

--- cogs/help.py
+++ cogs/help.py
@@ -15,20 +15,22 @@ class Help(commands.Cog):
             description="",
             color=discord.Color.blue()
         )
+        embed.add_field(name="```Attention```", value="Plusieurs fonctionnalitées poussées du bot nécessitent une inscription préalable avec /register, il est donc vivement conseillé de s'inscrire avant toute utilisation.")
         embed.add_field(
             name="Commandes Github",
             value=(
-                "`/register` • Permet de s'inscrire à la base de données du bot (Obligatoire pour les actions Github).\n"
-                "`/profile` • Permet d'afficher son profil et ses repository associés.\n"
-                "`/list_repos` • Permet de lister tous les repository disponibles à l'utilisateur.\n"
-                "`/add_repo` • Permet d'ajouter un repository à son profil et de définir un salon où recevoir les notifications suivantes :\n"
+                "`/profile` • Permet d'afficher son profil et ses dépôts associés.\n"
+                "`/list_repos` • Permet de lister tous les dépôts disponibles à l'utilisateur.\n"
+                "`/add_repo <utilisateur/dépôt>` • Permet d'ajouter un dépôt à son profil et de définir un salon où recevoir les notifications suivantes :\n"
                 " --- Commits\n"
                 " --- Pull request ouverte\n"
                 " --- Pull request fermée\n"
                 " --- Issue ouverte\n"
                 " --- Nouveau Fork\n"
                 " --- Publication de release\n"
-                "`/remove_repo` • Permet d'enlever un repository à son profil et de réinitialiser les paramètres."
+                "`/remove_repo <utilisateur/dépôt>` • Permet d'enlever un dépôt à son profil et de réinitialiser les paramètres."
+                "`/last_commits <utilisateur/dépôt> • Permet d'afficher les 5 derniers commits sur un dépôt."
+                "`/last_issues <utilisateur/dépôt>` • Permet d'afficher les 5 dernières issues d'un dépôt."
             ),
             inline=False
         )
